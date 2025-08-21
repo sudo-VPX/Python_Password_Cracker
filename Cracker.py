273710bea2a02300b1fb0715f288140b27ed89e4
Vikrant Pathania <vikrantpathania.2007@gmail.com>
2025-08-21 14:29:33 +0530

    Lab Free Time Work

--- Cracker.py
+++ Cracker.py
@@ -1,7 +1,12 @@
+#Library
+
 import time
+
+#Self Made Library
+
 import ASCII_Drawing
 
-ASCII_Drawing.FeroFy_Logo()
+# All Chracters Allowed Yet
 
 All_Char = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
@@ -9,4 +14,20 @@ All_Char = [
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' 
 ]
 
+# Main Logo Entrance 
+
+print ( "_" * 130 )
+ASCII_Drawing.FeroFy_Logo()
+print ( "_" * 130 )
+
+# Some Details
+
+print ( "User There It Is Free Tool For Now , Just You Need To Know A Little Bit Of Python :)" )
+print ( "" )
+
 Password = input("Enter Your Password ---> ")
+
+# Exit Pannel
+
+print("You Can Exit Now")
+Exit_Review = input("Your Experience Plz ------>")
