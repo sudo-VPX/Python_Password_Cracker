a2dc96f7fda63f46788e68190d5b7e7008795e5f
VPX <vikrantpathania.2007@gmail.com>
2025-08-22 03:38:18 +0530

    Good Night

--- Cracker.py
+++ Cracker.py
@@ -1,33 +1,81 @@
 #Library
 
 import time
+import string
 
 #Self Made Library
 
 import ASCII_Drawing
 
+# Default Variables
+
+K = 1000 #Thousand
+M = 1000000 #Million
+B = 1000000000 #Billion
+T = 1000000000000 #Trillion
+Q = 1000000000000000 #Quadrillion
+
+
+# Functions
+
+def Max_Attempts():
+    print("Length Of Password Is ----> " + str(Length_Password))
+    print("Total Words Allowed ----> " + str(All_Char_Len))
+    Total_Max_Attempts = All_Char_Len ** Length_Password
+    if Total_Max_Attempts >= Q:
+        Total_Max_Attempts = str(round(Total_Max_Attempts / Q, 2)) + " Q"
+    elif Total_Max_Attempts >= T:
+        Total_Max_Attempts = str(round(Total_Max_Attempts / T, 2)) + " T"
+    elif Total_Max_Attempts >= B:
+        Total_Max_Attempts = str(round(Total_Max_Attempts / B, 2)) + " B"
+    elif Total_Max_Attempts >= M:
+        Total_Max_Attempts = str(round(Total_Max_Attempts / M, 2)) + " M"
+    elif Total_Max_Attempts >= K:
+        Total_Max_Attempts = str(round(Total_Max_Attempts / K, 2)) + " K"
+    print("Total Max Attempts We Can Make (Approx) ----> " + str(Total_Max_Attempts))
+
 # All Chracters Allowed Yet
 
-All_Char = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' 
-]
+All_Char = list(string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation)
+All_Char_Len = len(All_Char)
+All_Char_Print = " , ".join(str(Char) for Char in All_Char)
 
 # Main Logo Entrance 
 
-print ( "_" * 130 )
+time.sleep(0.1)
 ASCII_Drawing.FeroFy_Logo()
-print ( "_" * 130 )
+time.sleep(0.1)
+print ( "_" * 100 )
+print("")
+time.sleep(0.1)
 
 # Some Details
 
-print ( "User There It Is Free Tool For Now , Just You Need To Know A Little Bit Of Python :)" )
-print ( "" )
+print ( "User There This Is a Free Tool For Now , Just You Need To Know A Little Bit Of Python Which Is Optionl Enjoy :)" )
+print("")
+print ( "All Characters Allowed ---> " + All_Char_Print )
+print("")
 
-Password = input("Enter Your Password ---> ")
+# Password Related
+
+print("")
+print ( "_" * 100 )
+print("")
+Password = str(input("Enter Your Password ---> "))
+Length_Password = len(Password)
+print("")
+print ( "_" * 100 )
+print("")
+Max_Attempts()
+
+# Main Cracker
+
+# Will Code Soon { Good Night }
 
 # Exit Pannel
 
+print("")
+print ( "_" * 100 )
+print("")
 print("You Can Exit Now")
-Exit_Review = input("Your Experience Plz ------>")
+Exit_Review = input("Your Experience Plz ------> ")
